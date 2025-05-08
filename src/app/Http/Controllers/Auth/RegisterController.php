80dbbc9f3fb6278b9564eb845dc5e5e22fa75219
h.shino2 <h.shino0561@gmail.com>
2025-05-08 10:38:29 +0900

    branch_v0.10：作業ブランチ変更およびリモートリポジトリへ反映

---
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+
+class RegisterController extends Controller
+{
+    public function showRegistrationForm()
+    {
+        return view('auth.pg01_register');
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        auth()->login($user);
+        return redirect('/mypage');
+    }
+}
